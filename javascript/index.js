d3e0da6306f4ec1ad6d1f2755662dc1cba5746ed
Sagar <dev.sagartmg@gmail.com>
2024-03-03 17:00:33 +0545

    loop

--- javascript/index.js
+++ javascript/index.js
@@ -1,96 +1,54 @@
-let students = [
-  {
-    name: "ram",
-    hasPaid: false,
-    hasScholorship: true,
-  },
-  {
-    name: "hari",
-    hasPaid: false,
-    hasScholorship: false,
-  },
-  {
-    name: "shyam",
-    hasPaid: true,
-    hasScholorship: false,
-  },
-];
+function findMultiplication(filterObj) {
+  /* 
+    object destructuring
 
-let users = ["ram", "hari", "shyam"];
+    let input = filterObj.input;
+    let startFrom = filterObj.startFrom;
+    let endAt = filterObj.endAt;
 
-/* console.log(users[0]);
-console.log(users[1]);
-console.log(users[2]);
- */
+   */
 
-for (let index = 0; index < 3; index++) {
-  console.log(index);
-  console.log(users[index]);
-}
+  let { input, startFrom, endAt } = filterObj;
 
-
-let todos = [
-  {
-    title: "html",
-    status: true,
-  },
-  {
-    title: "react",
-    status: false,
-  },
-  {
-    title: "express",
-    status: false,
-  },
-];
-
-
-
-
-for (let index = 0; index < 3; index++) {
-  if (todos[index].status) {
-    console.log(`${todos[index].title} is complted`);
-  } else {
-    console.log(`${todos[index].title} is pending`);
+  for (let startingPoint = startFrom; startingPoint <= endAt; startingPoint++) {
+    let result = input * startingPoint;
+    console.log(`${input} * ${startingPoint} = ${result} `);
   }
 }
 
+// findMultiplication(10, 5, 10);
+// findMultiplication(12, 1, 5);
 
-function findMultiplication(input){
+findMultiplication({
+  input: 10,
+  startFrom: 5,
+  endAt: 10,
+});
 
-  /* code to find multiplication of given input */
+findMultiplication({
+  input: 12,
+  startFrom: 1,
+  endAt: 5,
+});
 
-   /*  
-    5 * 1 = 5  to 5 * 10  = 50
-    5 * 1 = 5  to 5 * 10  = 50
-   
-   */
+let numbers = [1, 2, 3, 4, 5, 100, 200, 300];
+
+for (let index = 0; index < numbers.length; index++) {
+  numbers[index] = numbers[index] * 2;
 }
 
-findMultiplication(5)
-findMultiplication(10)
+// numbers[0] = numbers[0] * 2;
+// numbers[1] = numbers[1] * 2;
+// numbers[2] = numbers[2] * 2;
+// numbers[3] = numbers[3] * 2;
+// numbers[4] = numbers[4] * 2;
+
+/*  code here to double the element of number  */
+
+console.log(numbers); //expected output: [2,4,6,8,10]
 
 
-// console.log(`${todos[0].title} is ${todos[0].status}`);
-// console.log(`${todos[1].title} is ${todos[1].status}`);
-// console.log(`${todos[2].title} is ${todos[2].status}`);
 
-/* findMultiplication(5, 5, 10) */
-/* findMultiplication(10, 1, 5) */
 
-/* TODO: print each users using loop */
 
-/* 
-   loop  
-    - for  ( when we know number of iterations )
-    - while
-    - do while
-*/
 
-// for( startingPoint; condition; mutator ){
-
-for (let count = 1; count <= 5; count++) {
-  console.log({ count });
-}
-
-console.log("end of loop");
