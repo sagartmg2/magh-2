c823cc9bba960e73bdbe7478047e9cc7d44a9d6a
Sagar <dev.sagartmg@gmail.com>
2024-03-06 16:59:50 +0545

    dom manipulation

--- javascript/index.js
+++ javascript/index.js
@@ -31,7 +31,77 @@ findMultiplication({
   endAt: 5,
 });
 
-let numbers = [1, 2, 3, 4, 5, 100, 200, 300];
+let numbers = [1, 2, 3, 4, 5];
+numbers[0] = numbers[0] * 2;
+numbers[1] = numbers[1] * 2;
+numbers[2] = numbers[2] * 2;
+numbers[3] = numbers[3] * 2;
+numbers[4] = numbers[4] * 2;
+
+console.log(numbers); // [2,3,6,8,10]
+
+
+let brands = ["apple", "samsung", "lg", "sony"];
+
+let companies = [
+  {
+    name:"mindrisers",
+    url:"www.midrisers.com"
+  },
+  {
+    name:"leapfrog",
+    url:"www.leapfrog.com"
+  },
+  {
+    name:"cotiviti",
+    url:"www.cotivity.com"
+  },
+]
+
+
+/*
+  url of mindrisers is www.mindrisers.com
+  url of leapfrog is www.leapfrog.com
+  url of cotiviti is www.cotivity.com
+ */
+
+let users = [
+  {
+    name: "ram",
+    cartItems: [
+      {
+        product_id: 1,
+        price: 100,
+        quantity: 2,
+      },
+      {
+        product_id: 2,
+        price: 300,
+        quantity: 2,
+      },
+    ],
+  },
+  {
+    name: "hari",
+    cartItems: [
+      {
+        product_id: 3,
+        price: 100,
+        quantity: 2,
+      },
+      {
+        product_id: 4,
+        price: 300,
+        quantity: 2,
+      },
+    ],
+  },
+];
+
+/* ram has orderded of rs 800  */
+/* hari has orderded of rs ...  */
+
+
 
 for (let index = 0; index < numbers.length; index++) {
   numbers[index] = numbers[index] * 2;
@@ -46,9 +116,3 @@ for (let index = 0; index < numbers.length; index++) {
 /*  code here to double the element of number  */
 
 console.log(numbers); //expected output: [2,4,6,8,10]
-
-
-
-
-
-
