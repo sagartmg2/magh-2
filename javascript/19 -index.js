48a72df39bc0a827e5f419a24c9771a33f33101c
Sagar <dev.sagartmg@gmail.com>
2024-03-21 17:11:04 +0545

    filter,map

---
+++ javascript/19 -index.js
@@ -0,0 +1,90 @@
+let users = [
+  {
+    name: "ram",
+    cartItems: [
+      {
+        product: "watch",
+        quantity: 1,
+        rate: 1000,
+      },
+      {
+        product: "mouse",
+        quantity: -1,
+        rate: 2000,
+      },
+    ],
+  },
+  {
+    name: "hari",
+    cartItems: [
+      {
+        product: "keyboard",
+        quantity: -1, // ingore negative ones
+        rate: 2000,
+      },
+    ],
+  },
+];
+
+users.forEach((user) => {
+  let total = 0;
+
+  user.cartItems.forEach((item) => {
+    /*  if(item.quantity>0){
+      total = total + item.rate * item.quantity;
+    } */
+    total = total + (item.quantity > 0 ? item.quantity * item.rate : 0);
+  });
+
+  console.log(`${user.name} has orderded of rs:${total}`);
+});
+
+/* ram has order of rs:5000  */
+/* hari has order of rs: 2000  */
+
+/* ternary operator */
+/* just like forEach we have .filter , .map , .find .... */
+
+let todos = [
+  { title: "html", status: true },
+  { title: "css", status: true },
+  { title: "react", status: false },
+  { title: "express", status: false },
+];
+
+let completedTodos = [];
+
+
+
+/* 
+ completedTodos = [
+  { title: "html", status: true },
+  { title: "css", status: true },
+];
+*/
+// .filter() // reserach
+
+let modifiedTodos = []
+/* 
+ modifiedTodos = [
+  { title: "html", status: "compolted" },
+  { title: "css", status: "compolted" },
+  { title: "react", status: "pending" },
+  { title: "express", status: "pending" },
+]; */
+// .map() // reserach
+
+
+
+
+todos.forEach((todo) => {
+  /*
+   if (todo.status) {
+    console.log(`${todo.title} is compleed`);
+  } else {
+    console.log(`${todo.title} is pending`);
+  } 
+  */
+
+  console.log(`${todo.title} is ${todo.status ? "complted" : "pending"}`);
+});
--- javascript/index.js
+++ javascript/index.js
@@ -1,87 +1,121 @@
-let users = [
-  {
-    name: "ram",
-    cartItems: [
-      {
-        product: "watch",
-        quantity: 1,
-        rate: 1000,
-      },
-      {
-        product: "mouse",
-        quantity: -1,
-        rate: 2000,
-      },
-    ],
-  },
-  {
-    name: "hari",
-    cartItems: [
-      {
-        product: "keyboard",
-        quantity: -1, // ingore negative ones
-        rate: 2000,
-      },
-    ],
-  },
+let todos = [
+  { title: "html", status: true, createdAt: "2023", updatedAt: "2024" },
+  { title: "css", status: true, createdAt: "2023", updatedAt: "2024" },
+  { title: "react", status: false, createdAt: "2023", updatedAt: "2024" },
+  { title: "express", status: false, createdAt: "2023", updatedAt: "2024" },
 ];
 
-users.forEach((user) => {
-  let total = 0;
-
-  user.cartItems.forEach((item) => {
-    /*  if(item.quantity>0){
-      total = total + item.rate * item.quantity;
-    } */
-    total = total + (item.quantity > 0 ? item.quantity * item.rate : 0);
-  });
-
-  console.log(`${user.name} has orderded of rs:${total}`);
+let modifiedTodos = todos.map((element) => {
+  if (element.status) {
+    return {
+      title: element.title,
+      status: "completed",
+    };
+  } else {
+    return {
+      title: element.title,
+      status: "pending",
+    };
+  }
 });
 
-/* ram has order of rs:5000  */
-/* hari has order of rs: 2000  */
+modifiedTodos = todos.map((element) => {
+  if (element.status) {
+    return {
+      ...element,
+      status: "complted", // spread operator
+    };
+  } else {
+    return {
+      ...element,
+      status: "pending", // spread operator
+    };
+  }
+});
 
-/* ternary operator */
-/* just like forEach we have .filter , .map , .find .... */
+modifiedTodos = todos.map((el) => {
+  return {...el,status:el.status ? "compled":"pending" }
+});
 
-let todos = [
-  { title: "html", status: true },
-  { title: "css", status: true },
-  { title: "react", status: false },
-  { title: "express", status: false },
-];
+modifiedTodos = todos.map((el) => ({...el,status:el.status ? "compled":"pending" }));
 
-let completedTodos = [];
+console.log(modifiedTodos);
+
 /* 
- completedTodos = [
-  { title: "html", status: true },
-  { title: "css", status: true },
-];
-*/
-// .filter() // reserach
-
-let modifiedTodos = []
-/* 
- modifiedTodos = [
+expected output:
+let modifiedTodos = [
   { title: "html", status: "compolted" },
   { title: "css", status: "compolted" },
   { title: "react", status: "pending" },
   { title: "express", status: "pending" },
-]; */
-// .map() // reserach
+]
+ */
 
 
 
+let completedTodos = [];
 
-todos.forEach((todo) => {
-  /*
-   if (todo.status) {
-    console.log(`${todo.title} is compleed`);
-  } else {
-    console.log(`${todo.title} is pending`);
-  } 
-  */
+/* for (let index = 0; index < todos.length; index++) {
+  let element = todos[index];
+  if (element.status) {
+    completedTodos.push(element);
+  }
+} */
 
-  console.log(`${todo.title} is ${todo.status ? "complted" : "pending"}`);
+/* todos.forEach((element) => {
+  if (element.status) {
+    completedTodos.push(element);
+  }
+}); */
+
+completedTodos = todos.filter((element) => {
+  if (element.status) {
+    return true;
+  }
 });
+
+completedTodos = todos.filter((el) => el.status == true);
+completedTodos = todos.filter((el) => el.status);
+let pendingTodos = todos.filter((el) => !el.status);
+
+console.log({ completedTodos });
+
+let numbers = [1, 2, 3, 4, 5];
+let evenNumbers = numbers.filter((element, index) => {
+  console.log(element);
+  if (element % 2 == 0) {
+    return true;
+  }
+});
+
+evenNumbers = numbers.filter((el) => el % 2 == 0);
+
+let oddNumbers = numbers.filter((el) => {
+  if (el % 2 !== 0) {
+    return true;
+  }
+});
+
+oddNumbers = numbers.filter((el) => el % 2 !== 0);
+console.log(evenNumbers);
+console.log(oddNumbers);
+
+let doubledNumbers = numbers.map((element) => {
+  return element * 2;
+});
+
+let tripledNumbers = numbers.map((el) => el * 3);
+
+console.log({ doubledNumbers });
+console.log({ tripledNumbers });
+
+const double = (input) => {
+  return input * 2;
+};
+
+const triple = (input) => input * 3;
+const quadraple = (input) => input * 4;
+
+console.log(double(100));
+console.log(triple(100));
+console.log(quadraple(100));
